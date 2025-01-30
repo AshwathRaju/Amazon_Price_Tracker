735619523fb86a2ebc990e6b6059b4cc0be9ec13
AshwathRaju <ash1234raju@gmail.com>
2025-01-30 18:44:36 +0530

    Delete Price.js

--- Price.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const PriceSchema = new mongoose.Schema({
-    productUrl: { type: String, required: true },
-    price: { type: String, required: true },
-    date: { type: Date, default: Date.now }
-});
-
-module.exports = mongoose.model('Price', PriceSchema);

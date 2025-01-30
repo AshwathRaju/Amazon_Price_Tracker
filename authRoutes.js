9bed47ae84d26efcd946661f29ba2dfeeeca8f5d
AshwathRaju <ash1234raju@gmail.com>
2025-01-30 18:44:09 +0530

    Delete authRoutes.js

--- authRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const authController = require('Amazon-Price-Tracker/controllers/authController');
-
-const router = express.Router();
-
-router.post('/register', authController.register);
-router.post('/login', authController.login);
-
-module.exports = router;
